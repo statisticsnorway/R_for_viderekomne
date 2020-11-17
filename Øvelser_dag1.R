afb03897fdbaebf53df03eaef1293644b8cd530c
Jentoft <coo@ssb.no>
2020-11-17 09:00:47 +0100

    Added exercises for each day

---
+++ Øvelser_dag1.R
@@ -0,0 +1,99 @@
+# R for viderekomne
+#### DAG 1: Øvelse 1 ####
+
+# Bruk følgende vektor til øvelsene:
+byggtype <- c("Enebolig", "Våningshus", "Rekkehus", "Terrassehus")
+
+# 1) Bruk indeks for å plukke ut elementene 3 og 4 i vektoren
+
+
+
+
+# 2) Bruk indeks for å plukke ut alle bortsett fra element 4
+
+
+
+
+# 3) Endre "Rekkehus" i vektoren til "Andre bolig"
+
+
+
+
+# 4) bruk match for å finne indeks til "Rekkehus" i vektoren
+
+
+
+
+# 5) Lage en liste med vektoren byggtype (som brukt ovenfor) og bygningskodene 111, 113, 159, 135
+
+
+
+
+# 6) plukk ut kode for element 3 i byggkode fra listen du laget
+
+
+
+
+# 7) Endre listen til en tibble (datasett type i tidyverse)
+
+
+
+
+# 8) Bruk indekser for å plukke ut element som er på rad 2 i kolonn 1
+
+
+
+
+
+
+
+
+
+
+
+
+#########################################################################################
+#### DAG 1: Øvelse 2 ####
+
+# 1) Trekke en tilfeldig byggetype blant de fire i vektoren som du brukt i øvelse 1.
+
+
+
+
+# 2) Kjøre det samme tilfeldig trekking 3 ganger for å sjekke at du får forkjellige svar
+
+
+
+
+# 3) Bruk set.seed() og kjør tilfeldig trekking 3 ganger for å sjekke at du får samme svar
+
+
+
+
+# 4) Lage en vektor av lengde 4 med tilfeldig tall mellom 0 og 1.
+
+
+
+
+# 5) Sortere byggtype vektoren etter disse tilfeldige tall (stigende)
+# Hint: Bruk order og indeks
+
+
+
+
+# 6) Bruk if for å teste om "Enebolig" er inn i vektoren byggtype. 
+# Hvis det er, skriv ut melding "Enebolig er i vektoren". Hvis ikke, skriv en annen melding
+
+
+
+
+# 7) Lage en for-loop for å gå gjennom elementene i byggetype vektoren. Hvis elementen er "Enebolig", skriv ut
+#   "Dette er en enebolig". Hvis elementen ikke er "Enebolig" skriv ut en alternativ melding.  
+
+
+
+
+
+
+
+  
---
+++ Øvelser_dag2.R
@@ -0,0 +1,124 @@
+# Løsningsforslag for R for viderekomne
+#### DAG 2: Øvelse 3 ####
+
+# 1) Lage en funksjon med en parameter. Parameter skal være koden for byggetype. 
+# Funksjonen skal returnere "Enebolig" om koden er 111, ellers skal det returnere "Annen"
+# Bruk ifelse (ikke if og else) så at det kan ta imot vektorer
+
+
+
+
+
+
+# 2) Kalle funksjon for koden 111 og koden 133 for å sjekke at funksjonenen returnere riktig svar
+# Fungere det også for vektorer? dvs. c(111, 133)
+
+
+
+
+
+# 3) Les inn data for boligbygg (bolig.csv) fra 2019 med funksjon read_csv2 (tidyverse). 
+# Kjør funksjonen din på variabel byggetype
+# Bakgrunn: Data er fra Statbank 05940 for areal fullfort bygd per region etter bygningstype for 2019
+
+
+
+
+
+# 4) Kjør funksjon for å lage en ny variabel i datasett (Gjerne bruke tidyverse med pipelines)
+
+
+
+
+
+# 5) Lage en ny funksjon med to parameter. Det første parameter skal være det samme som før.
+# Den andre paremater skal ta "en" eller "nb". Denne skal styre hvilken språk skal returners. 
+# Skriv den nye funksjonen for å returnere engelsk "Detached house" eller "Other house" hvis "en" er spesifisert
+
+
+
+
+
+
+# 6) Test funksjonen på verdiene 111 og 113 med spesifisering av engelsk. 
+# Fungerer det også for å lage en ny variabel i bolig datasett ? 
+
+
+
+
+
+
+# 7) Lage den samme funksjonen med sette en default verdi for språk til "nb". 
+
+
+
+
+
+
+
+# 8) Skriv den samme funksjonen men legg til en varsling/stopp om en verdi som ikke er "en" eller "nb" er skrevet
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################################################################################
+#### DAG 2 - øvelse 4 ####
+
+# 1) Søk i klassR for en klassifikasjon for bygningstype
+# Hint: bruk funksjonen SearchKlass()
+
+
+
+
+# 2) Hent ut standard for bygningstype (klass = 31)
+# Hint: bruk funksjonen GetKlass()
+
+
+
+
+
+# 3) Lage en ny variabel med navn av byggetype
+# Hint: bruk ApplyKlass()
+# Hint: Du er fritt å bruke det direkte med $ for å lage ny variabel eller som en del av pipline
+
+
+
+
+
+
+# 4) Lage en variabel med engelsk navn for byggetype
+# Hint: bruk lang = "en" parameter i ApplyKlass()
+
+
+
+
+
+
+# 5) Lage en RMarkdown dokument om boligbygg 
+# Prøv å legge inn en tabell med kable
+# Lage en figur: for eksemepl søylediagram over total bruksareal fullførte i 2019 etter fylke 
+
+
+
+
+
+
+
+
+
